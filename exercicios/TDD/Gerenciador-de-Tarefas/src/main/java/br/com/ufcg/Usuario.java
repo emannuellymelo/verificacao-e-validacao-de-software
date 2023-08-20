515059f45ee5a035dda813dda27c42458fdd39e4
emannuellymelo <emannuelly.melo@ccc.ufcg.edu.br>
2023-08-20 17:04:49 -0300

    Implementacao do sistema de gerenciamento de tarefas

--- exercicios/TDD/Gerenciador-de-Tarefas/src/main/java/br/com/ufcg/Usuario.java
+++ exercicios/TDD/Gerenciador-de-Tarefas/src/main/java/br/com/ufcg/Usuario.java
@@ -1,2 +1,45 @@
-package br.com.ufcg;public class Usuario {
+package br.com.ufcg;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Usuario {
+
+    private String nome;
+    private ArrayList<Tarefa> tarefas;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+        this.tarefas = new ArrayList<>();
+    }
+
+    public void addTarefa(Tarefa tarefa) {
+        this.tarefas.add(tarefa);
+    }
+
+    public void editTarefa(String titulo, String descricao, String validade, String prioridade, String codigo) {
+        for(Tarefa t : tarefas) {
+            if(t.getCodigo().equals(codigo)) {
+                t.setTitulo(titulo);
+                t.setDescricao(descricao);
+                t.setPrioridade(prioridade);
+                t.setValidade(validade);
+            }
+        }
+    }
+
+    public void removeTarefa(String codigo) {
+        for(Tarefa t : tarefas) {
+            if(t.getCodigo().equals(codigo)) {
+                tarefas.remove(t);
+                break;
+            }
+        }
+    }
+
+    public ArrayList<Tarefa> getTarefas() {
+        Collections.sort(this.tarefas, new DataEPrioridadeComparator());
+        return tarefas;
+    }
 }
